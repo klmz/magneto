1cb6985232e82b256c5d2634d9ceadf4ba3e4ac2
Alex Nederlof <alex@nederlof.com>
2014-02-04 21:49:32 +0100

    Context should not be final

--- src/main/java/me/magnet/magneto/Context.java
+++ src/main/java/me/magnet/magneto/Context.java
@@ -1,13 +1,13 @@
 package me.magnet.magneto;
 
-import lombok.Value;
+import lombok.Getter;
 import lombok.experimental.Builder;
 
 /**
  * The context of a given command. This can be used in a method to retrieve extra information about the call.
  */
 @Builder
-@Value
+@Getter
 public class Context {
 
 	private User from;
